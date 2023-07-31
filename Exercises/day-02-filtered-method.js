b3ea368825628a219d85ae268acc09e067e8bf89
Fawaz@github12 <fawaz@provility.com>
2023-07-31 12:40:09 +0530

    minor change

--- Exercises/day-02-filtered-method.js
+++ Exercises/day-02-filtered-method.js
@@ -50,16 +50,39 @@
 
 
 
-const number = [1,3,5,7,6,2]
-function getEvenNumbers(num) {
-    let output = []
-    for(let i=0;i<num.length;i++){
-        if(num[i]%2 == 0){
-            output.push(num[i])
+// const number = [1,3,5,7,6,2]
+// function getEvenNumbers(num) {
+//     let output = []
+//     for(let i=0;i<num.length;i++){
+//         if(num[i]%2 == 1){
+//             output.push(num[i])
+//         }
+//     }
+//     return output
+// }
+//
+// const result3 = getEvenNumbers(number)
+// console.log(result3)
+
+const input = [1, 5, 7, 6, 5, 3];
+
+function findSecondLargest(arr) {
+    let largest = arr[0];
+    let secondLargest = arr[0];
+
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > largest) {
+            secondLargest = largest;
+            largest = arr[i];
+        } else if (arr[i] > secondLargest && arr[i] !== largest) {
+            secondLargest = arr[i];
         }
     }
-    return output
+
+    return secondLargest;
 }
 
-const result3 = getEvenNumbers(number)
-console.log(result3)
+const output = findSecondLargest(input);
+console.log("Second largest element:", output); // Output will be 6
+
+
